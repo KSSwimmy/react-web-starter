0494f7e7a344f2d22bf5f3cb19e1358f3dab350c
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2019-10-16 23:29:04 -0400

    :pencil: Completed and notes added
    Completed and notes added

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -8,7 +8,7 @@ export class Todo extends Component {
     super();
     this.state = {
       todos: [],
-      newTodo: ''
+      newTodo: '' // allows us to keep track of that state of the input
     }
   }
 
@@ -19,29 +19,36 @@ export class Todo extends Component {
   }
 
   handleClick(event) {
-    event.preventDefault();
+    event.preventDefault(); //because we're using a form we're using preventDefault 
     const todos = [...this.state.todos, this.state.newTodo];
-    this.setState({todos, newTodo: ''});
+    this.setState({todos, newTodo: ''}); // this is updating the todos state with the newTodo state
+  }
+
+  removeTodo(i) {
+    const todos = [...this.state.todos.slice(0,i), this.state.todos.slice(i + 1)]
+    this.setState({todos});
   }
  
   render() {
     return(
       <div> 
         <form> 
+          {/* form allows the use of the enter button */}
           <input
-          onChange={this.handleChange.bind(this)} // onChange allows you to type. 
+          onChange={this.handleChange.bind(this)} // onChange allows you to type (updates the input box). Without it the value is always going to be set to whatever the state is. In case it is an empty string. (It won't change)
           value={this.state.newTodo} //whatever is in the input box
           type=" text" 
           placeholder= "new todo"
           /> 
           <button 
           onClick={this.handleClick.bind(this)}>create</button> 
-          {/* onClick - clicking the button to make the todo show on the screen */}
+          {/* onClick - clicking the button to make the todo show on the screen. Adds that new todo to our todos collection */}
         </form>
         <ul> 
-          {this.state.todos.map(todo => <li>{todo}</li>)} 
-          {/* lists the new todos */}
+          {this.state.todos.map((todo, i) => (
+          <li onClick={() => this.removeTodo.call(this, i)}>{todo}</li>))} 
         </ul>
+        {/* lists the new todos */}
       </div>
     );
   }
